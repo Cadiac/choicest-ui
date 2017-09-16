d6a334d31382002b17a48052e851f682aac99638
Jaakko Husso <jaakko.husso@futurice.com>
2017-09-17 00:34:21 +0300

    Lets actually not use style-elements

--- src/View.elm
+++ src/View.elm
@@ -3,10 +3,6 @@ module View exposing (view)
 import Html exposing (Html, div, img, text)
 import Html.Attributes exposing (src)
 import Model exposing (..)
-import Style exposing (..)
-import Style.Color as Color
-import Style.Font as Font
-import Style.Shadow as Shadow
 
 
 view : Model -> Html msg
