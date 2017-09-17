8d5d31d27467f75e5aa8519599a1540bcce7b624
Jaakko Husso <jaakko.husso@futurice.com>
2017-09-17 18:00:07 +0300

    Add routing and couple of example routes

---
+++ src/Page/About.elm
@@ -0,0 +1,52 @@
+module Page.About exposing (Model, Msg, init, update, view)
+
+import Html exposing (Html, button, div, h1, h2, img, text)
+import Html.Attributes exposing (class, src)
+import Html.Events exposing (onClick)
+
+
+---- MODEL ----
+
+
+type alias Model =
+    { pageTitle : String
+    , pageBody : String
+    , counter : Int
+    }
+
+
+
+-- UPDATE --
+
+
+type Msg
+    = Decrement
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Decrement ->
+            ( { model | counter = model.counter - 1 }, Cmd.none )
+
+
+init : Model
+init =
+    Model "About" "This is the aboutpage" 1
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ img [ src "/logo.svg" ] []
+        , h1 [] [ text model.pageTitle ]
+        , h2 [] [ text model.pageBody ]
+        , text (toString model.counter)
+        , div []
+            [ button
+                [ onClick Decrement
+                , class "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+                ]
+                [ text "Button" ]
+            ]
+        ]
---
+++ src/Page/NotFound.elm
@@ -0,0 +1,13 @@
+module Page.NotFound exposing (view)
+
+import Html exposing (Html, div, h1, text)
+
+
+-- VIEW --
+
+
+view : Html msg
+view =
+    div []
+        [ h1 [] [ text "Not Found" ]
+        ]
