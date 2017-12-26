239920635ebbb66f93b37b18597f1d35c4469782
Jaakko Husso <jaakko.husso@futurice.com>
2017-12-26 02:08:58 +0200

    Play around with mdl styles

--- src/View/Page.elm
+++ src/View/Page.elm
@@ -1,7 +1,9 @@
 module View.Page exposing (ActivePage(..), layout)
 
+import Css exposing (..)
 import Data.Collection as Collection exposing (stringToSlug)
 import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (class, css, href, src)
 import Route exposing (Route)
 
 
@@ -17,27 +19,48 @@ isLoading can be used to slow loading during slow transitions
 -}
 layout : ActivePage -> Html msg -> Html msg
 layout page content =
-    div []
+    div
+        [ css
+            [ backgroundColor (rgb 92 92 92) ]
+        , class "mdl-layout mdl-js-layout"
+        ]
         [ viewHeader page
-        , div [] [ content ]
+        , main_ [ class "mdl-layout__content" ] [ content ]
         , viewFooter
         ]
 
 
 viewHeader : ActivePage -> Html msg
 viewHeader page =
-    nav []
-        [ div []
-            [ a [ Route.href Route.Home ]
-                [ text "Home" ]
-            , text " | "
-            , a [ Route.href Route.About ]
-                [ text "About" ]
-            , text " | "
-            , a [ Route.href (Route.Collection (stringToSlug "test-collection")) ]
-                [ text "Collection" ]
+    div []
+        [ header [ class "mdl-layout__header mdl-layout__header--transparent" ]
+            [ div [ class "mdl-layout__header-row" ]
+                [ span [ class "mdl-layout-title" ]
+                    [ text "Choicest" ]
+                , div [ class "mdl-layout-spacer" ]
+                    []
+                , nav [ class "mdl-navigation" ]
+                    [ a [ class "mdl-navigation__link", Route.href Route.Home ]
+                        [ text "Home" ]
+                    , a [ class "mdl-navigation__link", Route.href Route.About ]
+                        [ text "About" ]
+                    , a [ class "mdl-navigation__link", Route.href (Route.Collection (stringToSlug "test-collection")) ]
+                        [ text "Collection" ]
+                    ]
+                ]
             ]
-        , hr [] []
+        , div [ class "mdl-layout__drawer" ]
+            [ span [ class "mdl-layout-title" ]
+                [ text "Choicest" ]
+            , nav [ class "mdl-navigation" ]
+                [ a [ class "mdl-navigation__link", Route.href Route.Home ]
+                    [ text "Home" ]
+                , a [ class "mdl-navigation__link", Route.href Route.About ]
+                    [ text "About" ]
+                , a [ class "mdl-navigation__link", Route.href (Route.Collection (stringToSlug "test-collection")) ]
+                    [ text "Collection" ]
+                ]
+            ]
         ]
 
 
